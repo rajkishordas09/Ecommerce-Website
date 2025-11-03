97e4394a245276cdcec217db3040831d65f144d1
Raj kishor Das <rajkishordas.04150@gmail.com>
2025-11-03 17:07:46 +0530

    add authentication as a middleware

---
+++ Middleware/authN.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const authN = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(404).json({ error: "token is not provided" })
+    }
+    const token = authHeader.split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, 'my_secret_rajkishordas09')
+        req.username = decoded.username;
+        req.role = decoded.role;
+        next()
+    }
+    catch (err) {
+        res.status(404).json({ error: err.message })
+    }
+
+}
+module.exports = authN;
